d8a407c44391866e9d4424a3cb9544bfcb06acbf
franco-pro <francodjoutsoppro@gmail.com>
2024-05-17 10:34:58 +0100

    reset-project

--- frontend/app-example/(tabs)/_layout.tsx
+++ frontend/app-example/(tabs)/_layout.tsx
@@ -15,6 +15,7 @@ export default function TabLayout() {
         headerShown: false,
       }}
     >
+      <Tabs.Screen name="start_page" />
       <Tabs.Screen
         name="index"
         options={{
